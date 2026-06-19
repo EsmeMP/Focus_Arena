30f8661b2fe79296fda25a570228e1922b6e04c4
Esme Munoz <esmeralda.padilla.com@gmail.com>
2026-06-18 21:30:56 -0600

    01 Starting configuration

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:prueba_2/config/theme/app_theme.dart';
+import 'package:prueba_2/presentation/chat/chat_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mi aplicacion',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 3).theme(),
+      home: const ChatScreen()
+    );
+  }
+}
